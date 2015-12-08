99b9fb6b234791bc10e3b40dd501b87cbbaae48e
primetime00 <primetime00@users.noreply.github.com>
2015-12-08 11:40:45 -0500

    Added libGDX

---
+++ android/src/com/kegelapps/palace/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.kegelapps.palace.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.kegelapps.palace.Palace;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Palace(), config);
+	}
+}
---
+++ desktop/src/com/kegelapps/palace/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kegelapps.palace.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kegelapps.palace.Palace;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Palace(), config);
+	}
+}
